13ff5e3de57e046323515337927eded5ac756ad7
Siebert <arthur.siebert@atos.net>
2020-04-04 16:36:21 +0200

    ChatOverview update

--- Chatsystem/src/GUI/ChatOverview.java
+++ Chatsystem/src/GUI/ChatOverview.java
@@ -11,16 +11,27 @@ public class ChatOverview extends JFrame {
     private static JPanel pnl_chatOverview;
     private static JTextField searchbarJTextField = new JTextField();
 
+    public ChatOverview(){
+        this.getContentPane().add(pnl_chatOverview);
+        this.setSize(200, 800);
+        this.setVisible(true);
+    }
+
     public static void main(String[] args){
+        
         User[] testUser = new User[2];
         testUser[0] = new User("Kevin", "egal");
         testUser[1] = new User("Tom", "egal");
         JButton[] userButtons = new JButton[testUser.length];
         searchbarJTextField.setText("Suche...");
+        pnl_chatOverview = new JPanel(new GridLayout(testUser.length+1, 1));
+        pnl_chatOverview.add(searchbarJTextField);
         for(int i = 0;i<userButtons.length;i++){
+            userButtons[i] = new JButton();
             userButtons[i].setText(testUser[i].getKennung());
+            pnl_chatOverview.add(userButtons[i]);
         }
-        pnl_chatOverview = new JPanel(new GridLayout(3, 1));
+        ChatOverview newChatO = new ChatOverview();
     }
 
 }
