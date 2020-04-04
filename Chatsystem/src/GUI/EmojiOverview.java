04427e37aa60ce915765ef932ec04a66539443da
lorenzto <tobias.hockenheim@gmx.de>
2020-04-04 15:41:33 +0200

    Declare Initialize and add EmojiButtons to Panel

--- Chatsystem/src/GUI/EmojiOverview.java
+++ Chatsystem/src/GUI/EmojiOverview.java
@@ -2,81 +2,166 @@ package GUI;
 
     import javax.swing.*;
     import java.awt.GridLayout;
-
+    import java.awt.event.ActionEvent;
+    import java.awt.event.ActionListener;
 
 public class EmojiOverview extends JFrame{
 
-    
+
     private static JPanel pnl_emojisOverview;
-    private static JButton btn_emojiSweat;
-    private static JButton btn_emojiSmile;
-    private static JButton btn_emojiLaughTears;
-    private static JButton btn_emojiLaughTearsCrazy;
-    private static JButton btn_emojiRedHeart;
-    private static JButton btn_emojiThumbsUp;
-    private static JButton btn_emojiYellowHeart;
-    private static JButton btn_emojiTongueOut;
-    private static JButton btn_emojiSunglasses;
-    private static JButton btn_emojiMuscle;
-    private static JButton btn_emojiMoneyBag;
-    private static JButton btn_emojiHeartEyes;
-    private static JButton btn_emojiAngry;
-    private static JButton btn_emojiHundredPercent;
-    private static JButton btn_emojiZwinkerFace;
-    private static JButton btn_emojiBlush;
-    private static JButton btn_emojiShushingFace;
-    private static JButton btn_emojithinking;
-    private static JButton btn_emojiSad;
-    private static JButton btn_emojiBrokenHeart;
-    private static JButton btn_emojiPointLeftTone;
-    private static JButton btn_emojiSmiley;
-    private static JButton btn_emojiCallMe;
-    private static JButton btn_emojiGreyMoon;
-    private static JButton btn_emojiSunny;
+    private static JButton btn_relieved;
+    private static JButton btn_pray;
+    private static JButton btn_violin;
+    private static JButton btn_angry;
+    private static JButton btn_monocle;
+    private static JButton btn_point_up;
+    private static JButton btn_ok;
+    private static JButton btn_angel;
+    private static JButton btn_happy;
+    private static JButton btn_shush;
+    private static JButton btn_think;
+    private static JButton btn_money;
+    private static JButton btn_sweat;
+    private static JButton btn_loveeyes;
+    private static JButton btn_pensive;
+    private static JButton btn_fit;
+    private static JButton btn_brokenHeart;
+    private static JButton btn_rollingEyes;
+    private static JButton btn_inLove;
+    private static JButton btn_unhappy;
+    private static JButton btn_shruggin;
+    private static JButton btn_heart;
+    private static JButton btn_pointRight;
+    private static JButton btn_djinn;
+    private static JButton btn_grim;
+    private static JButton btn_cry;
+    private static JButton btn_moon;
+    private static JButton btn_pointLeft;
+    private static JButton btn_callMe;
+    private static JButton btn_mustache;
+    private static JButton btn_rofl;
+    private static JButton btn_thumb;
+    private static JButton btn_smile;
+    private static JButton btn_joy;
+    private static JButton btn_heartYellow;
+    private static JButton btn_heartBlack;
+    private static JButton btn_anotherSmile;
+    private static JButton btn_fullMoon;
+    private static JButton btn_grin;
+    private static JButton btn_sun;
+    private static JButton btn_thumbDown;
+    private static JButton btn_tongueOut;
+    private static JButton btn_100;
+    private static JButton btn_wink;
+    private static JButton btn_sunglasses;
+    private static JButton btn_glasses;
+    private static JButton btn_party;
+
+
 
 
 
     public EmojiOverview(){
 
         pnl_emojisOverview = new JPanel(new GridLayout(5, 10, 1, 1));
-        pnl_emojisOverview.add(btn_emojiRelieved);
-        pnl_emojisOverview.add(btn_emojiPrayTone);
-        pnl_emojisOverview.add(btn_emojiViolin);
-        pnl_emojisOverview.add(btn_emojiAngry);
-        pnl_emojisOverview.add(btn_emojiFaceWithMonocle);
-        pnl_emojisOverview.add(btn_emojiPointUp);
-        pnl_emojisOverview.add(btn_emojiOkHand);
-        pnl_emojisOverview.add(btn_emojiInnocent);
-        pnl_emojisOverview.add(btn_emojiBlush);
-        pnl_emojisOverview.add(btn_emojiShushingFace);
-        pnl_emojisOverview.add(btn_emojiThinking);
-        pnl_emojisOverview.add(btn_emojiMoneyBag);
-        pnl_emojisOverview.add(btn_emojiSweat);
-        pnl_emojisOverview.add(btn_emojiHeartEyes);
-        pnl_emojisOverview.add(btn_emojiSad);
-        pnl_emojisOverview.add(btn_emojiMuscle);
-        pnl_emojisOverview.add(btn_emojiBrokenHeart);
-        pnl_emojisOverview.add(btn_emojiRollingEyes);
-        pnl_emojisOverview.add(btn_emojiSmilingFaceWithThreeHearts);
-        pnl_emojisOverview.add(btnemojiWorried);
-        pnl_emojisOverview.add(btn_emojiWomanShrugging);
-        pnl_emojisOverview.add(btn_emojiRedHeart);
-        pnl_emojisOverview.add(btn_emojiPointRight);
-        pnl_emojisOverview.add(btn_emojiManGenie);
-        pnl_emojisOverview.add(btn_emojiGrimacing);
-        pnl_emojisOverview.add(btn_emojiSob);
-        pnl_emojisOverview.add(btn_emojiMoonWithFace);
-        pnl_emojisOverview.add(btn_emojiPointLeftTone);
-        pnl_emojisOverview.add(btn_emojiCallMe);
-        pnl_emojisOverview.add(btn_emojiMan);
-        pnl_emojisOverview.add(btn_emojiRofl);
-        pnl_emojisOverview.add(btn_emojiThumbsUp);
-        pnl_emojisOverview.add(btn_emojiBlush);
-        pnl_emojisOverview.add(btn_emojiRollingEyes);
-        pnl_emojisOverview.add(btn_emojiSmiley);
-        pnl_emojisOverview.add(btn_emojiJoy);
-        pnl_emojisOverview.add(btn_emojiYellowHeart);
-        pnl_emojisOverview.add(btn_emojiSmile);
+
+        //Initialize Buttons in Grid
+        btn_relieved = new JButton(emojiFinder.emojis[0]);
+        btn_pray = new JButton(emojiFinder.emojis[1]);
+        btn_violin= new JButton(emojiFinder.emojis[2]);
+        btn_angry= new JButton(emojiFinder.emojis[3]);
+        btn_monocle= new JButton(emojiFinder.emojis[4]);
+        btn_point_up = new JButton(emojiFinder.emojis[5]);
+        btn_ok = new JButton(emojiFinder.emojis[6]);
+        btn_angel = new JButton(emojiFinder.emojis[7]);
+        btn_happy = new JButton(emojiFinder.emojis[8]);
+        btn_shush = new JButton(emojiFinder.emojis[9]);
+        btn_think = new JButton(emojiFinder.emojis[10]);
+        btn_money = new JButton(emojiFinder.emojis[11]);
+        btn_sweat = new JButton(emojiFinder.emojis[12]);
+        btn_loveeyes = new JButton(emojiFinder.emojis[13]);
+        btn_pensive = new JButton(emojiFinder.emojis[14]);
+        btn_fit = new JButton(emojiFinder.emojis[15]);
+        btn_brokenHeart = new JButton(emojiFinder.emojis[16]);
+        btn_rollingEyes = new JButton(emojiFinder.emojis[17]);
+        btn_inLove = new JButton(emojiFinder.emojis[18]);
+        btn_unhappy = new JButton(emojiFinder.emojis[19]);
+        btn_shruggin = new JButton(emojiFinder.emojis[20]);
+        btn_heart = new JButton(emojiFinder.emojis[21]);
+        btn_pointRight = new JButton(emojiFinder.emojis[22]);
+        btn_djinn = new JButton(emojiFinder.emojis[23]);
+        btn_grim = new JButton(emojiFinder.emojis[24]);
+        btn_cry = new JButton(emojiFinder.emojis[25]);
+        btn_moon = new JButton(emojiFinder.emojis[26]);
+        btn_pointLeft = new JButton(emojiFinder.emojis[27]);
+        btn_callMe = new JButton(emojiFinder.emojis[28]);
+        btn_mustache = new JButton(emojiFinder.emojis[29]);
+        btn_rofl = new JButton(emojiFinder.emojis[30]);
+        btn_thumb = new JButton(emojiFinder.emojis[31]);
+        btn_smile = new JButton(emojiFinder.emojis[32]);
+        btn_joy = new JButton(emojiFinder.emojis[33]);
+        btn_heartYellow = new JButton(emojiFinder.emojis[34]);
+        btn_heartBlack= new JButton(emojiFinder.emojis[35]);
+        btn_anotherSmile = new JButton(emojiFinder.emojis[36]);
+        btn_fullMoon = new JButton(emojiFinder.emojis[37]);
+        btn_grin = new JButton(emojiFinder.emojis[38]);
+        btn_sun = new JButton(emojiFinder.emojis[39]);
+        btn_thumbDown = new JButton(emojiFinder.emojis[40]);
+        btn_tongueOut = new JButton(emojiFinder.emojis[41]);
+        btn_100 = new JButton(emojiFinder.emojis[42]);
+        btn_wink = new JButton(emojiFinder.emojis[43]);
+        btn_sunglasses = new JButton(emojiFinder.emojis[44]);
+        btn_glasses = new JButton(emojiFinder.emojis[45]);
+        btn_party = new JButton(emojiFinder.emojis[46]);
+
+        //Add Buttons to Panel
+        pnl_emojisOverview.add(btn_relieved);
+        pnl_emojisOverview.add(btn_pray);
+        pnl_emojisOverview.add(btn_violin);
+        pnl_emojisOverview.add(btn_angry);
+        pnl_emojisOverview.add(btn_monocle);
+        pnl_emojisOverview.add(btn_point_up);
+        pnl_emojisOverview.add(btn_ok);
+        pnl_emojisOverview.add(btn_angel);
+        pnl_emojisOverview.add(btn_happy);
+        pnl_emojisOverview.add(btn_shush);
+        pnl_emojisOverview.add(btn_think);
+        pnl_emojisOverview.add(btn_money);
+        pnl_emojisOverview.add(btn_sweat);
+        pnl_emojisOverview.add(btn_loveeyes);
+        pnl_emojisOverview.add(btn_pensive);
+        pnl_emojisOverview.add(btn_fit);
+        pnl_emojisOverview.add(btn_brokenHeart);
+        pnl_emojisOverview.add(btn_rollingEyes);
+        pnl_emojisOverview.add(btn_inLove);
+        pnl_emojisOverview.add(btn_unhappy);
+        pnl_emojisOverview.add(btn_shruggin);
+        pnl_emojisOverview.add(btn_heart);
+        pnl_emojisOverview.add(btn_pointRight);
+        pnl_emojisOverview.add(btn_djinn);
+        pnl_emojisOverview.add(btn_grim);
+        pnl_emojisOverview.add(btn_cry);
+        pnl_emojisOverview.add(btn_moon);
+        pnl_emojisOverview.add(btn_pointLeft);
+        pnl_emojisOverview.add(btn_callMe);
+        pnl_emojisOverview.add(btn_mustache);
+        pnl_emojisOverview.add(btn_rofl);
+        pnl_emojisOverview.add(btn_thumb);
+        pnl_emojisOverview.add(btn_smile);
+        pnl_emojisOverview.add(btn_joy);
+        pnl_emojisOverview.add(btn_heartYellow);
+        pnl_emojisOverview.add(btn_heartBlack);
+        pnl_emojisOverview.add(btn_anotherSmile);
+        pnl_emojisOverview.add(btn_fullMoon);
+        pnl_emojisOverview.add(btn_grin);
+        pnl_emojisOverview.add(btn_sun);
+        pnl_emojisOverview.add(btn_thumbDown);
+        pnl_emojisOverview.add(btn_tongueOut);
+        pnl_emojisOverview.add(btn_100);
+        pnl_emojisOverview.add(btn_wink);
+        pnl_emojisOverview.add(btn_sunglasses);
+        pnl_emojisOverview.add(btn_glasses);
+        pnl_emojisOverview.add(btn_party);
         
 
 
@@ -87,6 +172,113 @@ public class EmojiOverview extends JFrame{
 
     public static void main(String args[]){
 
+       //Add ActionListener for each emojibutton
+        btn_relieved.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_pray.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_violin.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_angry.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_monocle.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_point_up.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_ok.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_angel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_happy.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_shush.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_think.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_money.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_sweat.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_loveeyes.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+        btn_pensive.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+        });
+
+    
     }
 
 }
