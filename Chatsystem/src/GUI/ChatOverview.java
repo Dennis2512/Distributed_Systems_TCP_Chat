64c99a016943391f41c8aff0d0f4218990cc6ad6
Siebert <arthur.siebert@atos.net>
2020-04-04 14:53:42 +0200

    Overview Init

--- Chatsystem/src/GUI/ChatOverview.java
+++ Chatsystem/src/GUI/ChatOverview.java
@@ -1,9 +1,24 @@
 package GUI;
 
+import basisversion.server.*;
+import lukastests.server.User;
+
 import javax.swing.*;
 
 
-public class ChatOverview {
+public class ChatOverview extends JFrame {
     private static JPanel pnl_chatOverview;
+    JTextField searchbarJTextField = new JTextField();
+
+    public ChatOverview(){
+        User[] testUser = new User[2];
+        testUser[0] = new User("Kevin", "egal");
+        testUser[1] = new User("Tom", "egal");
+        JButton[] userButtons = new JButton[testUser.length];
+        searchbarJTextField.setText("Suche...");
+        for(int i = 0;i<userButtons.length;i++){
+            userButtons[i].setText(testUser[i].getKennung());
+        }
+    }
 
 }
